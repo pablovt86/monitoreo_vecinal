b1d1d8346709c00f222343c0b75fa1f097c2e653
pablovt <pablo.vt@hotmail.com.ar>
2026-04-12 00:41:44 -0300

    se agrega path

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
-
+const path = require('path')
 app.use(cors())
 app.use(express.json())
 app.use('/api/uploads', express.static(path.join(__dirname, 'uploads')));
